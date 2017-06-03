fb261e959c031596bd37528b3267eea485ef21e5
Bruno Rafael <bruninho_lito@hotmail.com>
2017-06-03 18:58:05 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header id="header">
+      <div>oiii</div>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+body{color: blue; 
+  #header{ 
+    div{
+      color: red;
+     } 
+  }
+}
+</style>
